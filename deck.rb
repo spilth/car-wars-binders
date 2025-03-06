7a1204c534b3a20c46f8405ad25a1c4566a21468
Brian Kelly <brian@spilth.org>
2025-03-05 19:14:33 -0600

    Make placeholder cards look more like real cards

--- deck.rb
+++ deck.rb
@@ -10,11 +10,15 @@ types.each do |type|
 
   Squib::Deck.new(cards: type_indexes.size, width: 825, height: 1125) do
     background color: 'white'
-    # rect width: 825, height: 1125, stroke_color: 'black'
 
-    text str: data['Type'].values_at(*type_indexes), x: 100, y: 300, font: 'Arial 16', width: 625, align: :center
-    text str: data['Cost'].values_at(*type_indexes), x: 100, y: 500, font: 'Arial 16', width: 625, align: :center
-    text str: data['Name'].values_at(*type_indexes), x: 100, y: 700, font: 'Arial 16', width: 625, height: 400, wrap: :word_char, align: :center
+    name_alignments = data['Kind'].values_at(*type_indexes).map { |t| t == "Build" ? :left : :right }
+    name_x = data['Kind'].values_at(*type_indexes).map { |t| t == "Build" ? 100 : 200 }
+
+    text str: data['Build Cost'].values_at(*type_indexes), x: 100, y: 100, font: 'Arial 20', width: 625, align: :right
+    text str: data['Crew Cost'].values_at(*type_indexes), x: 100, y: 100, font: 'Arial 20', width: 625, align: :left
+    text str: data['Name'].values_at(*type_indexes), x: name_x, y: 50, font: 'Arial 14', width: 525, height: 250, wrap: :word_char, align: name_alignments, valign: :bottom
+    text str: data['Type'].values_at(*type_indexes), x: 100, y: 300, font: 'Arial 10', width: 625, align: name_alignments
+    text str: data['Card Set'].values_at(*type_indexes), x: 100, y: 1000, font: 'Arial 10', width: 625, height: 400, wrap: :word_char, align: :right
 
     save_sheet prefix: "cards_#{type}_", columns: 4, rows: 4, margin: 20, gap: 3
   end
