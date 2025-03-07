d5144d05cd68ea55d3391777f4904ea1e1419f37
Brian Kelly <brian@spilth.org>
2025-03-07 14:58:24 -0600

    More layout cleanup. Include Subtype.

--- deck.rb
+++ deck.rb
@@ -11,11 +11,13 @@ types.each do |type|
   Squib::Deck.new(cards: type_indexes.size, width: 825, height: 1125) do
     background color: 'white'
 
-    text str: data['Build Cost'].values_at(*type_indexes), x: 200, y: 200, font: 'Arial 20', width: 625, align: :center
-    text str: data['Crew Cost'].values_at(*type_indexes), x: 200, y: 200, font: 'Arial 20', width: 625, align: :center
-    text str: data['Name'].values_at(*type_indexes), x: 200, y: 400, font: 'Arial Bold 16', width: 625, height: 300, wrap: :word_char, align: :center
-    text str: data['Type'].values_at(*type_indexes), x: 200, y: 600, font: 'Arial 14', width: 625, align: :center
-    text str: data['Card Set'].values_at(*type_indexes), x: 200, y: 800, font: 'Arial 12', width: 625, height: 400, wrap: :word_char, align: :center
+    text str: data['Kind'].values_at(*type_indexes), x: 200, y: 200, font: 'Arial 16', width: 625, align: :center
+    text str: data['Build Cost'].values_at(*type_indexes), x: 400, y: 200, font: 'Arial 16', width: 525, align: :center
+    text str: data['Crew Cost'].values_at(*type_indexes), x: 100, y: 200, font: 'Arial 16', width: 525, align: :center
+    text str: data['Name'].values_at(*type_indexes), x: 200, y: 300, font: 'Arial Bold 18', width: 625, height: 400, wrap: :word_char, align: :center
+    text str: data['Type'].values_at(*type_indexes), x: 200, y: 700, font: 'Arial 14', width: 625, align: :center
+    text str: data['Subtype'].values_at(*type_indexes), x: 200, y: 800, font: 'Arial 14', width: 625, align: :center
+    text str: data['Card Set'].values_at(*type_indexes), x: 200, y: 900, font: 'Arial 12', width: 625, align: :center
 
     save_sheet prefix: "cards_#{type}_", columns: 4, rows: 4, margin: 0, gap: 0
   end
